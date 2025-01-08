b488653187e481cfd9140c0a6ea4624788b05014
nhattinh2601 <nhattinh2601@gmail.com>
2025-01-08 09:44:52 +0700

    notification in database

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/deposit', [App\Http\Controllers\DepositController::class,'deposit'])->name('deposit');
+Route::get('/mark-as-read', [App\Http\Controllers\DepositController::class,'markAsRead'])->name('mark-as-read');
